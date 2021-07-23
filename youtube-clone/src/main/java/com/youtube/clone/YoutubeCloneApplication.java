4a6b04dd8d9349c90da464bdca291758c2d64187
erick <erickjhorman@gmail.com>
2021-07-23 14:33:46 -0500

    first commit

---
+++ youtube-clone/src/main/java/com/youtube/clone/YoutubeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeCloneApplication.class, args);
+    }
+
+}
---
+++ youtube-clone/src/test/java/com/youtube/clone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
